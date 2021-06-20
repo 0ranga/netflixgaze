c40f894c696a0af3fc0fc1f6a5ae4a03c1705aa0
mercvre <studiomercvre@outlook.com>
2021-06-20 02:10:17 +0200

    Cleaned up CSS and p5 sketch to make it resizable

--- js/sketch.js
+++ js/sketch.js
@@ -2,16 +2,24 @@
 'use strict';
 
 var img;
+let parent;
 
 function preload() {
   img = loadImage('img/logo-netflix.jpg');
+  parent = document.getElementById('myContainer');
 }
 
 function setup() {
-  let myCanvas = createCanvas(1920, 700);
+  img.resize(parent.offsetWidth, 0);
+  let myCanvas = createCanvas(parent.offsetWidth, img.height);
   myCanvas.parent('myContainer');
   image(img, 0, 0);
+}
 
+function windowResized() {
+  img.resize(parent.offsetWidth, 0);
+  resizeCanvas(parent.offsetWidth, img.height);
+  image(img, 0, 0);
 }
 
 function draw() {
@@ -27,10 +35,10 @@ function draw() {
   set(x2, y2, get(x1, y1, w, h));
 }
 
-function keyReleased() {
-  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
-  if (keyCode == DELETE || keyCode == BACKSPACE) {
-    clear();
-    image(img, 0, 0);
-  }
-}
+// function keyReleased() {
+//   if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+//   if (keyCode == DELETE || keyCode == BACKSPACE) {
+//     clear();
+//     image(img, 0, 0);
+//   }
+// }
